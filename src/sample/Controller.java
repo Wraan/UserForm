f0b716e3e5b7c381d2c7a8c4f42e38675092c87a
Kamil Drewniak <kamil.drewniak13@gmail.com>
2018-03-07 16:24:46 +0100

    Adding editing and save to file options

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,7 +3,22 @@ package sample;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Controller {
 
@@ -17,22 +32,22 @@ public class Controller {
     private Label ageErrorLabel;
 
     @FXML
-    private Label nameErrorLabel;
+    private Label lastNameErrorLabel;
 
     @FXML
-    private Label fornameErrorLabel;
+    private Label firstNameErrorLabel;
 
     @FXML
     private TextField ageTextF;
 
     @FXML
-    private TextField nameTextF;
+    private TextField lastNameTextF;
 
     @FXML
-    private TextField fornameTextF;
+    private TextField firstNameTextF;
 
     @FXML
-    private Label fornameLabel;
+    private Label firstNameLabel;
 
     @FXML
     private Label ageLabel;
@@ -41,11 +56,28 @@ public class Controller {
     private ListView<String> list;
 
     @FXML
-    private Label nameLabel;
+    private Label lastNameLabel;
 
     @FXML
     private MenuItem deleteMenuItem;
 
+    @FXML
+    public void initialize() {
+        readFromFile();
+
+    }
+    void readFromFile(){
+        try(BufferedReader br = new BufferedReader(new FileReader("records.txt"))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                list.getItems().add(line);
+                line = br.readLine();
+            }
+        }catch (Exception e){
+            System.out.println("Nie znaleziono pliku");
+        }
+    }
 
     @FXML
     void checkIfNumber(KeyEvent key) {
@@ -83,15 +115,15 @@ public class Controller {
     }
 
     private void clearFields(){
-        fornameTextF.clear();
-        nameTextF.clear();
+        firstNameTextF.clear();
+        lastNameTextF.clear();
         ageTextF.clear();
     }
 
     private void clearErrors(){
         ageErrorLabel.setText("");
-        nameErrorLabel.setText("");
-        fornameErrorLabel.setText("");
+        lastNameErrorLabel.setText("");
+        firstNameErrorLabel.setText("");
     }
 
     @FXML
@@ -106,25 +138,23 @@ public class Controller {
         }
         if(c == 13){
             if(!tf.getText().trim().equals("")) {
-                if (tf.getId().equals("fornameTextF"))
-                    nameTextF.requestFocus();
-                if (tf.getId().equals("nameTextF"))
+                if (tf.getId().equals("firstNameTextF"))
+                    lastNameTextF.requestFocus();
+                if (tf.getId().equals("lastNameTextF"))
                     ageTextF.requestFocus();
             }
-            else{
+            else
                 textFieldEmptyError(tf);
-            }
-
         }
     }
 
     private void textFieldEmptyError(TextField tf) {
-        if (tf.getId().equals("fornameTextF"))
-            fornameErrorLabel.setText("Pole nie może być puste!");
+        if (tf.getId().equals("firstNameTextF"))
+            firstNameErrorLabel.setText("Pole nie może być puste!");
 
-        if (tf.getId().equals("nameTextF")) {
-            nameErrorLabel.setText("Pole nie może być puste!");
-        }
+        if (tf.getId().equals("nameTextF"))
+            lastNameErrorLabel.setText("Pole nie może być puste!");
+
     }
 
     private boolean checkForWhitespaces(StringBuilder sb, TextField tf){
@@ -142,13 +172,85 @@ public class Controller {
     @FXML
     void addRecordToList() {
         clearErrors();
+
+        String userAdd = getUserFromFields();
+        if(userAdd == null)
+            return;
+
+        if(!list.getItems().contains(userAdd)) {
+            list.getItems().add(list.getItems().size(), userAdd);
+            clearFields();
+            firstNameTextF.requestFocus();
+            list.getSelectionModel().clearSelection();
+
+        }
+        else {
+            firstNameErrorLabel.setText("Już jest na liscie");
+            clearFields();
+            firstNameTextF.requestFocus();
+        }
+
+    }
+
+    @FXML
+    void deleteRecordFromList() {
+        list.getItems().remove(list.getSelectionModel().getSelectedItem());
+        list.getSelectionModel().clearSelection();
+    }
+
+    @FXML
+    void exitApplication() {
+        saveToFile();
+        Platform.exit();
+    }
+
+    @FXML
+    void moveToTextFields(){
+        if(list.getSelectionModel().getSelectedItem() == null)
+            return;
+
+        String text = list.getSelectionModel().getSelectedItem().toString();
+        List<String> list;
+        list = Arrays.asList(text.split(" "));
+
+        firstNameTextF.setText(list.get(0));
+        lastNameTextF.setText(list.get(1));
+        ageTextF.setText(list.get(2));
+
+    }
+
+    @FXML
+    void editRecordFromList(){
+        clearErrors();
+
+        String userAdd = getUserFromFields();
+        if(userAdd == null)
+            return;
+
+        if(!list.getItems().contains(userAdd)) {
+            if(list.getSelectionModel().getSelectedIndex() != -1)
+                list.getItems().set(list.getSelectionModel().getSelectedIndex(), userAdd);
+            else
+                list.getItems().add(userAdd);
+
+            clearFields();
+            firstNameTextF.requestFocus();
+            list.getSelectionModel().clearSelection();
+        }
+        else {
+            firstNameErrorLabel.setText("Już jest na liscie");
+            clearFields();
+            firstNameTextF.requestFocus();
+        }
+    }
+    private String getUserFromFields(){
         StringBuilder sb = new StringBuilder();
 
-        if(!checkForWhitespaces(sb,fornameTextF))
-            return;
+        if(!checkForWhitespaces(sb,firstNameTextF))
+            return null;
 
-        if(!checkForWhitespaces(sb,nameTextF))
-            return;
+        if(!checkForWhitespaces(sb,lastNameTextF))
+            return null;
 
         int age = -1;
         try{
@@ -158,23 +260,27 @@ public class Controller {
             ageTextF.setText("");
         }
         if(!validAge(age)){
-            return;
+            return null;
         }
         sb.append(Integer.toString(age));
+        return sb.toString();
 
-        list.getItems().add(list.getItems().size(), sb.toString());
-        clearFields();
-        fornameTextF.requestFocus();
     }
-
     @FXML
-    void deleteRecordFromList() {
-        list.getItems().remove(list.getSelectionModel().getSelectedItem());
-    }
+    void saveToFile(){
+        List<String> stringList = list.getItems().subList(0, this.list.getItems().size());
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("records.txt");
+            for(String s: stringList){
+                writer.println(s);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        writer.close();
 
-    @FXML
-    void exitApplication() {
-        Platform.exit();
+
     }
 
 }
